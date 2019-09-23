6e97d113e77e0160f026b3614f16ebd0df30e7ad
Vadim <v_a_d@inbox.ru>
2019-09-23 10:57:08 +0300

    added modal window logic

--- src/feedListRender.js
+++ src/feedListRender.js
@@ -1,9 +1,12 @@
+import $ from 'jquery/dist/jquery';
+// eslint-disable-next-line no-unused-vars
+import modal from 'bootstrap';
+
 export default (a, b, newValue) => {
   const { feedTitle, feedDescription, feedPosts } = newValue[newValue.length - 1];
 
   const feedList = document.getElementById('Feeds list');
   const pTag = document.createElement('p');
-
   pTag.textContent = `${feedTitle} - ${feedDescription}`;
   feedList.appendChild(pTag);
 
@@ -11,14 +14,30 @@ export default (a, b, newValue) => {
 
   feedPosts.forEach((post) => {
     const postTitle = post.querySelector('title').textContent;
+    const postDescription = post.querySelector('description').textContent;
     const postLink = post.querySelector('link').textContent;
 
-    const aTag = document.createElement('a');
-    const brTag = document.createElement('br');
+    const divElement = document.createElement('div');
+    divElement.classList.add('mb-2', 'p-1');
 
+    const aTag = document.createElement('a');
+    aTag.classList.add('m-2');
     aTag.href = postLink;
     aTag.textContent = postTitle;
-    postsList.appendChild(aTag);
-    postsList.appendChild(brTag);
+
+    const bottonTag = document.createElement('botton');
+    bottonTag.setAttribute('type', 'botton', 'float-none');
+    bottonTag.classList.add('btn', 'btn-outline-info', 'btn-sm');
+    bottonTag.textContent = 'Description';
+    bottonTag.addEventListener('click', () => {
+      const feedDescriptionModal = document.querySelector('#feedDescriptionModal .modal-body');
+      feedDescriptionModal.textContent = postDescription;
+
+      $('#feedDescriptionModal').modal('show');
+    });
+
+    divElement.appendChild(aTag);
+    divElement.appendChild(bottonTag);
+    postsList.appendChild(divElement);
   });
 };
