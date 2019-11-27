62dbc1f406dd705a9a50817e5d13a47da998edfc
Vadim <v_a_d@inbox.ru>
2019-11-27 23:00:37 +0300

    Set up the renders

--- src/renderers/postsListRender.js
+++ src/renderers/postsListRender.js
@@ -1,18 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import modal from 'bootstrap';
 
-export default (_prop, _action, newPosts) => {
+export default (_prop, _action, allPosts, oldPosts) => {
   const postsList = document.getElementById('Posts list');
   const postListTitle = postsList.querySelector('p');
-
-  newPosts.forEach(({ postTitle, postDescription, postLink }) => {
+  const newPosts = allPosts.slice(-(allPosts.length - oldPosts.length));
+  newPosts.forEach(({ title, description, link }) => {
     const divElement = document.createElement('div');
     divElement.classList.add('mb-2', 'p-1');
 
     const aTag = document.createElement('a');
     aTag.classList.add('m-2');
-    aTag.href = postLink;
-    aTag.textContent = postTitle;
+    aTag.href = link;
+    aTag.textContent = title;
 
     const bottonTag = document.createElement('botton');
     bottonTag.setAttribute('type', 'botton');
@@ -22,7 +22,7 @@ export default (_prop, _action, newPosts) => {
     bottonTag.textContent = 'Description';
     bottonTag.addEventListener('click', () => {
       const feedDescriptionModal = document.querySelector('#feedDescriptionModal .modal-body');
-      feedDescriptionModal.textContent = postDescription;
+      feedDescriptionModal.textContent = description;
     });
 
     divElement.append(aTag);
