b37fa5a5ee648ef945f2802dd712b0a3c630d18e
Vadim <v_a_d@inbox.ru>
2020-03-18 15:46:18 +0300

    add i18next

--- src/renderers/errorRender.js
+++ src/renderers/errorRender.js
@@ -1,8 +1,19 @@
+import i18next from 'i18next';
+import resources from '../locales';
+
+(async () => {
+  await i18next.init({
+    lng: 'ru',
+
+    resources,
+  });
+})();
+
 export default (_prop, _action, newError) => {
   const errorDiv = document.createElement('div');
   errorDiv.classList.add('alert', 'alert-danger');
   errorDiv.setAttribute('role', 'alert');
-  errorDiv.textContent = newError;
+  errorDiv.textContent = i18next.t(`errors.${newError}`);
 
   const addFeedForm = document.querySelector('.jumbotron');
   const container = addFeedForm.parentNode;
